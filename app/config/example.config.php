14cb16b0b4f2854e29101dfb35393d3f5049843f
Dat Le Thac <datquynhvinh1231@gmail.com>
2023-04-29 18:16:23 +0700

    Set config

---
+++ app/config/example.config.php
@@ -0,0 +1,16 @@
+<?php
+    //App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    //URL Root
+    define('URLROOT', 'http://localhost/Figure-planet');
+    //Site Name
+    define('SITENAME', 'Figure-planet');
+    //App Version
+    define('APPVERSION', '0.2.0');
+
+    //Database Configuration
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'figureplanet');
+    define('DB_CHARSET', 'utf8');
